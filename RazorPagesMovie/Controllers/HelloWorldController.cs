1ff984fdd7073f0684d00882c6c9f249130afe4d
Mark Kpamy <markkpamy@gmail.com>
2018-04-15 18:52:51 +0200

    commit 2

---
+++ RazorPagesMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorPagesMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
